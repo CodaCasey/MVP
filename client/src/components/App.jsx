c7b521e1c17cdc9c78acb825ecbf0b5b7da355f9
Casey Jackson <ctjacks3@gmail.com>
2020-07-25 19:56:32 -0700

    this commit will make a skeleton

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component{
+ constructor(props){
+   super(props)
+
+ }
+  <div>eagle beak</div>
+}
+
+export default App;
---
+++ database/Seed.js
@@ -0,0 +1,10 @@
+//practice file for seeding and testing data
+
+let container = [
+
+  {english:'hello', spanish: 'hola', num:3},
+  {english:'dog', spanish: 'perro', num:2},
+  {english:'', spanish: 'hola', num:3},
+  {english:'hello', spanish: 'hola', num:3},
+
+]
