d294e5961ee89c0340dedb44ec355b8ca1d4e32a
Casey Jackson <ctjacks3@gmail.com>
2020-07-27 22:11:52 -0700

    this commit will add a getNewCard function for a weighted random card based on schedule

--- client/src/components/FlashCard.jsx
+++ client/src/components/FlashCard.jsx
@@ -11,13 +11,13 @@ class FlashCard extends React.Component{
   this.state={
     words:[
     {id: 1, english:'hello', spanish: 'hola', factor:3, schedule:3,repeat:false},
-    {id:2, english:'dog', spanish: 'perro', factor:3, schedule:3,repeat:false},
-    {id:3, english:'but', spanish: 'pero', factor:3, schedule:3,repeat:false},
-    {id:4, english:'goodbye', spanish: 'adios', factor:3, schedule:3,repeat:false},
+    {id:2, english:'dog', spanish: 'perro', factor:3, schedule:5,repeat:false},
+    {id:3, english:'but', spanish: 'pero', factor:3, schedule:1,repeat:false},
+    {id:4, english:'goodbye', spanish: 'adios', factor:3, schedule:2,repeat:false},
     {id:5, english:'thank you', spanish: 'gracias', factor:3, schedule:3,repeat:false},
-    {id:6, english:'why', spanish: 'por que', factor:3, schedule:3,repeat:false},
-    {id:7, english:'what', spanish: 'que', factor:3, schedule:3,repeat:false},
-    {id:8, english:'mirror', spanish: 'el espejo', factor:3, schedule:3,repeat:false}
+    {id:6, english:'why', spanish: 'por que', factor:3, schedule:4,repeat:false},
+    {id:7, english:'what', spanish: 'que', factor:3, schedule:2,repeat:false},
+    {id:8, english:'mirror', spanish: 'el espejo', factor:3, schedule:1,repeat:false}
 
     ],
 
@@ -84,8 +84,6 @@ updateWordStats(event){
    let lastSchedule=2; // The duration of last review space.
    let lastFactor=2 // The factor that was used to caculate last schedule.
    let updateInfo  = supermemo2(1, words[index].schedule, words[index].factor)
-    console.log(words[index])
-   console.log(updateInfo)
 
    //===================
    const itemIndex = this.state.words.findIndex(item => item.id === words[index].id )
@@ -98,22 +96,33 @@ updateWordStats(event){
    words: copyArray,
 
    });
-   console.log(words[index])
-   console.log(words)
-   //===================
-  //  this.setState({
-  //    words[index].schedule:updateInfo.schedule,
-  //    words[index].factor:updateInfo.factor,
-  //    words[index].repeate:updateInfo.repeate
-
-  //  })
-  //  console.log(words[index])
-
 }
 
-mathFunc(){
+mathFunc(event){
+  const{words,index} = this.state;
+  let weightTotal = 0;
 
+  for (let i = 0; i < words.length; i++) {
+    let trueWeight=Math.abs(5 - words[i].schedule);
+      weightTotal += trueWeight;
+  }
+  for (let i = 0; i < words.length; i++) {
+    let trueWeight=Math.abs(5 - words[i].schedule);
+      words[i].distribution = trueWeight / weightTotal;
+  }
 
+  function getNewCard(list) {
+      let key = 0;
+      let selector = Math.random();
+      while (selector > 0) {
+          selector -= list[key].distribution;
+          key++;
+      }
+      key--;
+      return list[key];
+  }
+
+  console.log(getNewCard(words));
 
 }
 
@@ -125,16 +134,6 @@ mathFunc(){
    let foreignWord=words[index].spanish;
    let nativeWord=words[index].english;
    let numFactor=words[index].num;
-   //console.log(words[index])
-
-   let quality; // A number between 0 and 5 that indicate the quality of review. 0 is the worse while 5 is the best.
-   let lastSchedule=2; // The duration of last review space.
-   let lastFactor=2 // The factor that was used to caculate last schedule.
-   let updateInfo  = supermemo2(1, words[index].schedule, words[index].factor)
-
-   console.log(updateInfo)
-
-
 
    return(
     <div className="flashcard-container">
@@ -162,7 +161,7 @@ mathFunc(){
       </div>
       <div>
         <button onClick={this.updateWordStats}>next question</button>
-        <button onClick={this.moveBackward}>prev question</button>
+        <button onClick={this.mathFunc}>prev question</button>
       </div>
 
 
